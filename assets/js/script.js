a0faac5d2152fe2042afcf4bd2cc25d63b4853c9
MatheusTesterr <matheusoliveira112002@gmail.com>
2023-04-04 22:29:41 -0300

    Commit 04/04

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 
 document.querySelector("#qtde").addEventListener("change", calcularOrcamneto)
 document.querySelector("#js").addEventListener("change", calcularOrcamneto)
+document.querySelector("#layout_sim").addEventListener("change", calcularOrcamneto)
+document.querySelector("#layout_nao").addEventListener("change", calcularOrcamneto)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamneto)
+
 
 function calcularOrcamneto() {
     let qtde = document.querySelector("#qtde").value
@@ -9,11 +13,17 @@ function calcularOrcamneto() {
 
     let js = document.querySelector("#js").checked
 
-    if (js == true) {
-        preco = preco + (preco * 10/100)
-    }
+    
+        if (js) preco *= 1.1
 
-    document.querySelector("#preco").innerHTML = "R$" + preco
+        let layout = document.querySelector("#layout_sim").checked
+        if(layout) preco += 500
+
+        let prazo = document.querySelector("#prazo").value
+        document.querySelector("#label_prazo").innerHTML = "Prazo(" + prazo + " semanas)"
+    
+
+    document.querySelector("#preco").innerHTML = "R$" + preco.toFixed(2)
 }
 
 //manioulação do DOM
